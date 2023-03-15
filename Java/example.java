0a3d4935d1d3aa2e979e9e8ae68350866ca5c336
Suffering-Cephalopod <Neil-Kasson@users.noreply.github.com>
2023-03-15 19:34:00 -0400

    This is a git commit

---
+++ Java/example.java
@@ -0,0 +1,5 @@
+public class example{
+	public static void main(String[]args){
+		System.out.println("\n\t+-----------------------+\n\t|                       |\n\t|  This is an example.  |\n\t|                       |\n\t+-----------------------+");
+	}
+}
